864566b234b3e820e675bfdfc1092438f6e6c5ce
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-11-13 21:42:12 +0100

    rename file

--- run.bat
+++ run.bat
@@ -4,6 +4,8 @@ setlocal enabledelayedexpansion
 :: Get the current directory
 set CURRENT_DIR=%cd%
 
+title connectionInternat
+
 :: Run the binary with arguments
 :: "%CURRENT_DIR%\alacritty-portable.exe" --class=connectionInternat --title "Connection Internat" --config-file=NUL --command %CURRENT_DIR%\$EXE_NAME
 cmd /C %CURRENT_DIR%\$EXE_NAME
