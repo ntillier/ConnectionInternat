cea7f70035afd2261214f98c509c07cce9b26537
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-11-13 21:21:56 +0100

    actually no need for alacritty!

--- run.bat
+++ run.bat
@@ -5,4 +5,5 @@ setlocal enabledelayedexpansion
 set CURRENT_DIR=%cd%
 
 :: Run the binary with arguments
-"%CURRENT_DIR%\alacritty-portable.exe" --class=connectionInternat --title "Connection Internat" --config-file=NUL --command %CURRENT_DIR%\$EXE_NAME
+:: "%CURRENT_DIR%\alacritty-portable.exe" --class=connectionInternat --title "Connection Internat" --config-file=NUL --command %CURRENT_DIR%\$EXE_NAME
+cmd /C %CURRENT_DIR%\$EXE_NAME
